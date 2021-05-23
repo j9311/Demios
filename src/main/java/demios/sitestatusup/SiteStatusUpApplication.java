1d204908c6787fea2bb71258fc2d97876290e73d
j9311 <avilapost93@gmail.com>
2021-05-23 14:10:08 -0700

    spring boot java proj

---
+++ src/main/java/demios/sitestatusup/SiteStatusUpApplication.java
@@ -0,0 +1,13 @@
+package demios.sitestatusup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiteStatusUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiteStatusUpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/demios/sitestatusup/SiteStatusUpApplicationTests.java
@@ -0,0 +1,13 @@
+package demios.sitestatusup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiteStatusUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
